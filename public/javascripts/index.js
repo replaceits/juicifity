5cbbd6d64e38d0034f74bbd6b79eefe2ba485093
replaceits <sidwil0790@students.ecpi.edu>
2017-12-10 22:35:39 -0500

    Add parsePrecision function

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,23 +1,33 @@
 document.addEventListener("DOMContentLoaded", function() {
-    function indexViewModel() {
+    var valuePrecision = 2;
+
+    var parsePrecision = function parseNumberToFloatValueWithSetPrecision(num, precision) {
+        return Number(
+            parseFloat(num).toFixed(
+                (precision == undefined ? valuePrecision : precision)
+            )
+        );
+    }
+
+    var indexViewModel = function indexViewModel() {
         this.nicotineBase = ko.observable(100);
         this.targetNicotine = ko.observable(3);
         this.batchSize = ko.observable(10);
 
         this.resultNicotineSolutionML = ko.computed(function() {
-            return parseFloat(this.targetNicotine() / this.nicotineBase() * this.batchSize()).toFixed(2);
+            return parsePrecision(this.targetNicotine() / this.nicotineBase() * this.batchSize());
         }, this);
 
         this.resultNicotineSolutionPercent = ko.computed(function() {
-            return parseFloat(this.resultNicotineSolutionML() / this.batchSize() * 100).toFixed(2);
+            return parsePrecision(this.resultNicotineSolutionML() / this.batchSize() * 100);
         }, this);
 
         this.resultVGML = ko.computed(function() {
-            return parseFloat(this.batchSize() - (this.targetNicotine() / this.nicotineBase() * this.batchSize())).toFixed(2);
+            return parsePrecision(this.batchSize() - (this.targetNicotine() / this.nicotineBase() * this.batchSize()));
         }, this);
 
         this.resultVGPercent = ko.computed(function() {
-            return parseFloat(this.resultVGML() / this.batchSize() * 100).toFixed(2);
+            return parsePrecision(this.resultVGML() / this.batchSize() * 100);
         }, this);
     };
 
