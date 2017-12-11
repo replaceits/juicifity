9acea155b5efa756a3ff071543d4b1acaf8d47bf
replaceits <sidwil0790@students.ecpi.edu>
2017-12-11 01:57:12 -0500

    Add flavorings, implement LESS

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -9,11 +9,57 @@ document.addEventListener("DOMContentLoaded", function() {
         );
     }
 
+    var flavorIndex = 1;
+    var Flavor = function flavor(batchSize) {
+        this.batchSize = batchSize;
+
+        this.name = ko.observable("");
+        this.percent = ko.observable(0);
+        this.ml = ko.computed(function() {
+            return parsePrecision(this.batchSize() * this.percent() / 100);
+        }, this);
+
+        this.index = "Flavor " + flavorIndex++;
+    }
+
     var indexViewModel = function indexViewModel() {
+        // Base
         this.nicotineBase = ko.observable(100);
         this.targetNicotine = ko.observable(3);
         this.batchSize = ko.observable(10);
 
+        //Flavorings
+        this.flavors = ko.observableArray([]);
+
+        this.noFlavors = ko.computed(function() {
+            return (this.flavors().length == 0 ? "No flavors!" : "");
+        }, this);
+
+        this.addFlavor = function addFlavor() {
+            this.flavors.push(new Flavor(this.batchSize));
+        }
+
+        this.totalFlavorML = ko.computed(function totalFlavorML() {
+            var totalML = 0;
+            
+            for (var i = 0; i < this.flavors().length; i++) {
+                totalML += parseFloat(this.flavors()[i].ml());
+            }
+
+            return totalML;
+        }, this);
+
+        this.totalFlavorPercent = ko.computed(function totalFlavorML() {
+            var totalPercent = 0;
+            
+            for (var i = 0; i < this.flavors().length; i++) {
+                totalPercent += parseFloat(this.flavors()[i].percent());
+            }
+
+            return totalPercent;
+        }, this);
+
+        // Results
         this.resultNicotineSolutionML = ko.computed(function() {
             return parsePrecision(this.targetNicotine() / this.nicotineBase() * this.batchSize());
         }, this);
@@ -23,12 +69,20 @@ document.addEventListener("DOMContentLoaded", function() {
         }, this);
 
         this.resultVGML = ko.computed(function() {
-            return parsePrecision(this.batchSize() - (this.targetNicotine() / this.nicotineBase() * this.batchSize()));
+            return parsePrecision(this.batchSize() - this.resultNicotineSolutionML() - this.totalFlavorML());
         }, this);
 
         this.resultVGPercent = ko.computed(function() {
             return parsePrecision(this.resultVGML() / this.batchSize() * 100);
         }, this);
+
+        this.resultFlavorsML = ko.computed(function() {
+            return parsePrecision(this.totalFlavorML());
+        })
+
+        this.resultFlavorsPercent = ko.computed(function() {
+            return parsePrecision(this.totalFlavorPercent());
+        })
     };
 
     ko.applyBindings(indexViewModel);    
